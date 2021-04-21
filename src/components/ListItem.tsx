2567ce475e8eba2af2b64fb21320b8364031cd55
Brittney <brittney.cavazos@gmail.com>
2021-04-21 11:16:27 -0700

    style(ListItemComponent)added type and weaknesses section with pill styling

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import {setTypeColor} from '../services'
 
 interface PkmnProps{
     pokemon: Pokemon[];
@@ -17,7 +18,7 @@ const ListItem: React.FC<PkmnProps>= ({pokemon}) => {
                                 className='list-group-item d-flex justify-content-around align-items-center'
                                 key={index}
                             >
-                  {/* section img pulled to left */}
+                    {/* section img pulled to left */}
                                 <img src={poke.img} alt={poke.name} />
                                 <div className='poke-info'>
                                     <h2>
@@ -26,11 +27,40 @@ const ListItem: React.FC<PkmnProps>= ({pokemon}) => {
                                         </Link>
                                     </h2>
                                     <div>
-                                        <small>Height: {poke.height}</small>
+                                        <small>Height: {poke.height}</small><br/>
                                         <small>Weight: {poke.weight}</small>
                                     </div>
                                 </div>
                   {/* section pokemon name that is wrapped in a link */}
+                                <div className='type-info'>
+                                    <div>
+                                        <small>Type:</small><br/>
+                                        {poke.type.map((t, i) => {
+                                            return (
+                                                <span
+                                                className='badge badge-pill text-white mx-1'
+                                                style={{ backgroundColor: setTypeColor(t) }}
+                                                key={i}>
+                                                {t}
+                                                </span>
+                                            );
+                                            })}
+                                    </div>
+                                    <div>
+                                        <small>Weaknesses:</small><br/>
+                                        {poke.weaknesses.map((w,i)=>{
+                                            return(
+                                                <span 
+                                                className="badge badge-pill text-white mx-1"
+                                                style={{backgroundColor: setTypeColor(w)}}
+                                                key={i}
+                                                >
+                                                    {w}
+                                                </span>
+                                            );
+                                        })}
+                                    </div>
+                                </div>
                   {/* section for details under the pokemon name */}
                             </li>
                         );
